806ab8572b88b18dec7d9422c28622ee90590bf3
cesarjk78 <cesar.gonzales.t@tecsup.edu.pe>
2026-03-12 10:30:57 -0500

    Frontend conectado a Railway

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+    baseURL: "https://catalogo-productos-production-566c.up.railway.app/api",
 });
 
 export default api;
